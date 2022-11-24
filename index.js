99964b8803ddd7c1295d97ec8a78104a6abcc188
mbelasergesedi <mbelaserge@aksantimed.com>
2022-11-24 14:21:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-const Vonage = require('@vonage/server-sdk')
+exports.sms = async (request, res) => {
+    const Vonage = require('@vonage/server-sdk')
 
-const vonage = new Vonage({
-  apiKey: "586a6f9f",
-  apiSecret: "UIsdpWKqRe4JSDZb"
-})
+    const vonage = new Vonage({
+        apiKey: "586a6f9f",
+        apiSecret: "UIsdpWKqRe4JSDZb"
+    })
 
-const from = "Vonage APIs"
-const to = "32487757115"
-const text = 'A text message sent using the Vonage SMS API'
+    const from = "Vonage APIs"
+    const to = "32487757115"
+    const text = 'A text message sent using the Vonage SMS API'
 
-vonage.message.sendSms(from, to, text, (err, responseData) => {
-    if (err) {
-        console.log(err);
-    } else {
-        if(responseData.messages[0]['status'] === "0") {
-            console.log("Message sent successfully.");
+    vonage.message.sendSms(from, to, text, (err, responseData) => {
+        if (err) {
+            console.log(err);
         } else {
-            console.log(`Message failed with error: ${responseData.messages[0]['error-text']}`);
+            if (responseData.messages[0]['status'] === "0") {
+                console.log("Message sent successfully.");
+            } else {
+                console.log(`Message failed with error: ${responseData.messages[0]['error-text']}`);
+            }
         }
-    }
-})
+    })
+}
